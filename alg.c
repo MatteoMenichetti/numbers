e403140ef9aac4d2f6e089362bea73299d2ae514
matteomenichetti <matteowoozie@gmail.com>
2021-02-25 18:14:28 +0100

    Delete unused code

--- alg.c
+++ alg.c
@@ -11,18 +11,6 @@ void writeP(Number *p, int fd) {
     dprintf(fd, "%d\n", p->n);
 }
 
-/*Number *create(long i) {
-    if (i <= 2)exit(0);
-    Number *number = (Number *) malloc(sizeof(Number)), *p = number;
-
-    for (long j = 2; j < i; j++) {
-        p->n = j;
-        p->next = malloc(sizeof(Number));
-        p = p->next;
-    }
-    return number;
-}*/
-
 void crivello(Number *number) {
     int fd = open("primesMiddleExec.txt", O_WRONLY | O_TRUNC | O_CREAT, 0666);
     Number *prec = number;
