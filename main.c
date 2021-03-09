f596ba99b2c1c010faa7509fd7db3f100ab2951a
Rasperry MatteoMenichetti <matteowoozie@gmail.com>
2021-03-09 21:36:16 +0100

    insert control in main for input

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "alg.h"
 
 int main(int argc, char *argv[]) {
-    int n = 100;
-    Number *p = crivello(createPartOf(2, n));
-    do {
-        printf("%d", p->n);
-        p = p->next;
-    } while (p->next != NULL);
+    if (argc != 2) {
+        printf("insert one natural number");
+        exit(0);
+    }
+    if (atoi(argv[1]) > 2) {
+        Number *p = crivello(createPartOf(2, atoi(argv[1])));
+        do {
+            printf("%d, ", p->n);
+            p = p->next;
+        } while (p->next != NULL);
+    } else {
+        printf("Not valid number (%d is <= 2)", atoi(argv[1]));
+        exit(0);
+    }
 
     return 0;
 }
